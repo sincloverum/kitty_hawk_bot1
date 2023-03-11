318ec54d7ed7c10a7d36d942a726145ef3a9223d
X5G814 <chromis.biz@gmail.com>
2023-03-11 17:57:58 +0200

    Апгрейд сообщения

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Привет!\nЭто группа анонимных алкоголиков Китти Хок!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+    await message.reply("Привет!\nЭто группа анонимных алкоголиков Китти Хок!\nЭто тестовый запуск нашего робота.")
 
 
 @dp.message_handler()
