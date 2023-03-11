72e6b1d59387c489da00d255b2c8ef49e52a039d
gizmo555 <chromis.biz@gmail.com>
2023-03-11 16:04:35 +0200

    Echo test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
 
+API_TOKEN = '5602787567:AAGYv7NrSjwyW7qPs_yvu70C060zrcfZDbQ'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет!\nЭто группа анонимных алкоголиков Китти Хок!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
